eab174b11542084fceeb9e1d7e78c826eb58871e
dchattop <91043054+dchattop@users.noreply.github.com>
2022-03-23 12:07:04 +0530

    Update settings.py

--- RestApi_POC_django/settings.py
+++ RestApi_POC_django/settings.py
@@ -89,6 +89,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'RestApi_POC_django.wsgi.application'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
